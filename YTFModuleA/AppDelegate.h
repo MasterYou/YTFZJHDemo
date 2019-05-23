da837a4f7174cc2c919747409c9e300184f7d0a6
MasterYou <37956383@qq.com>
2019-05-23 15:22:47 +0800

    Initial Commit

---
+++ YTFModuleA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YTFModuleA
+//
+//  Created by larkdata on 2019/5/23.
+//  Copyright © 2019 com.zhihaitou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YTFModuleA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YTFModuleA
+//
+//  Created by larkdata on 2019/5/23.
+//  Copyright © 2019 com.zhihaitou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
